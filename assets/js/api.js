bd26c2b92e4e8fe10dad32eb6388a7c045eea5af
DominMFD <134434652+DominMFD@users.noreply.github.com>
2023-11-02 15:40:36 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/DominMFD/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/DominMFD/Portfolio/main/data/profile.json';
     const fetching = await fetch(url)
     const profileData = await fetching.json()
     return profileData
